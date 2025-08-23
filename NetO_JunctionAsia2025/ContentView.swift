1cdea6ecbb06e2e5bf8fd8e7bdde13b06e17f5ee
jiwonpark831 <jiwonpark831@gmail.com>
2025-08-23 13:32:01 +0900

    setting: setting

---
+++ NetO_JunctionAsia2025/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetO_JunctionAsia2025
+//
+//  Created by jiwon on 8/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetO_JunctionAsia2025/NetO_JunctionAsia2025App.swift
@@ -0,0 +1,17 @@
+//
+//  NetO_JunctionAsia2025App.swift
+//  NetO_JunctionAsia2025
+//
+//  Created by jiwon on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct NetO_JunctionAsia2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
