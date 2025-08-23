a2371060615ad41ebbf9cc0fe739807dc618395b
jiwonpark831 <jiwonpark831@gmail.com>
2025-08-23 13:48:18 +0900

    feat: 파이어베이스 연동

--- NetO_JunctionAsia2025/NetO_JunctionAsia2025App.swift
+++ NetO_JunctionAsia2025/NetO_JunctionAsia2025App.swift
@@ -5,10 +5,27 @@
 //  Created by jiwon on 8/23/25.
 //
 
+import Firebase
 import SwiftUI
 
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//
+//    return true
+//  }
+//}
+
 @main
-struct NetO_JunctionAsia2025App: App {
+struct NetOApp: App {
+
+    init() {
+        FirebaseApp.configure()
+    }
+
+    //    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
