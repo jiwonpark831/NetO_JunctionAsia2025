b583ad61ffc134a4695560a8b1be01361da0983b
jiwonpark831 <jiwonpark831@gmail.com>
2025-08-23 21:38:22 +0900

    feat: 뷰별 파일 생성

---
+++ NetO_JunctionAsia2025/Feature/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  NetO_JunctionAsia2025
+//
+//  Created by jiwon on 8/23/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ NetO_JunctionAsia2025/Feature/MakeHouseView.swift
@@ -0,0 +1,18 @@
+//
+//  MakeHouseView.swift
+//  NetO_JunctionAsia2025
+//
+//  Created by jiwon on 8/23/25.
+//  사용자 입력값 받는 뷰
+
+import SwiftUI
+
+struct MakeHouseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MakeHouseView()
+}
